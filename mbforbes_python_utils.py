ded39e9a0546720e51067220b64b1f2311a1e959
Maxwell Forbes <mbforbes@cs.washington.edu>
2019-10-09 18:18:52 -0700

    Add mturk util. Also now requires pandas so it's slow. Sorry.

--- mbforbes_python_utils.py
+++ mbforbes_python_utils.py
@@ -4,6 +4,8 @@ import code  # code.interact(local=dict(globals(), **locals()))
 import os
 from typing import List, Tuple, Dict, Set, Any, Optional, Callable
 
+import pandas as pd
+
 
 def read(path: str) -> str:
     """Returns contents of file at `path`, leading/trailing whitespace stripped."""
@@ -18,6 +20,33 @@ def write(path: str, contents: str, info_print: bool = True) -> None:
         f.write(contents)
     if info_print:
         print('Wrote {} chars to "{}"'.format(len(contents), path))
+
+
+def print_mturk_cols(path: str) -> None:
+    """Prints mturk columns for csv file at `path`.
+
+    NOTE: May want to make command line util at some point. Have to figure out how to do
+    this.
+    """
+    df = pd.read_csv(path)
+    inputs, outputs, admin = [], [], []
+    for c in df.columns:
+        if c.startswith('Input.'):
+            inputs.append(c)
+        elif c.startswith('Answer.'):
+            outputs.append(c)
+        else:
+            admin.append(c)
+
+    print('Admin:')
+    print(', '.join(sorted(admin)))
+    print()
+    print('Inputs:')
+    for i in sorted(inputs):
+        print(f'\t{i}')
+    print('Outputs:')
+    for o in sorted(outputs):
+        print(f'\t{o}')
 
 
 def main() -> None:
