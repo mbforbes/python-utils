35f3b653a202072d8897b24d061536b88498890a
Maxwell Forbes <m.b.forbes@gmail.com>
2019-12-13 16:03:17 -0800

    allow for writing to local files

--- mbforbes_python_utils.py
+++ mbforbes_python_utils.py
@@ -9,14 +9,16 @@ import pandas as pd
 
 def read(path: str) -> str:
     """Returns contents of file at `path`, leading/trailing whitespace stripped."""
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.read().strip()
 
 
 def write(path: str, contents: str, info_print: bool = True) -> None:
     """Writes contents to path, makes dirs if needed, prints info msg w/ path."""
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-    with open(path, 'w') as f:
+    dirname = os.path.dirname(path)
+    if dirname != "":
+        os.makedirs(dirname, exist_ok=True)
+    with open(path, "w") as f:
         f.write(contents)
     if info_print:
         print('Wrote {} chars to "{}"'.format(len(contents), path))
@@ -31,22 +33,22 @@ def print_mturk_cols(path: str) -> None:
     df = pd.read_csv(path)
     inputs, outputs, admin = [], [], []
     for c in df.columns:
-        if c.startswith('Input.'):
+        if c.startswith("Input."):
             inputs.append(c)
-        elif c.startswith('Answer.'):
+        elif c.startswith("Answer."):
             outputs.append(c)
         else:
             admin.append(c)
 
-    print('Admin:')
-    print(', '.join(sorted(admin)))
+    print("Admin:")
+    print(", ".join(sorted(admin)))
     print()
-    print('Inputs:')
+    print("Inputs:")
     for i in sorted(inputs):
-        print(f'\t{i}')
-    print('Outputs:')
+        print(f"\t{i}")
+    print("Outputs:")
     for o in sorted(outputs):
-        print(f'\t{o}')
+        print(f"\t{o}")
 
 
 def main() -> None:
