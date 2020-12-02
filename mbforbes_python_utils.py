aa215d4f1e47c412998359ecd63f9b72bf774b6b
Maxwell Forbes <m.b.forbes@gmail.com>
2020-12-01 16:18:09 -0800

    up to pypi, fly away little bird

--- mbforbes_python_utils.py
+++ mbforbes_python_utils.py
@@ -4,8 +4,6 @@ import code  # code.interact(local=dict(globals(), **locals()))
 import os
 from typing import List, Tuple, Dict, Set, Any, Optional, Callable
 
-import pandas as pd
-
 
 def read(path: str) -> str:
     """Returns contents of file at `path`, leading/trailing whitespace stripped."""
@@ -22,33 +20,6 @@ def write(path: str, contents: str, info_print: bool = True) -> None:
         f.write(contents)
     if info_print:
         print('Wrote {} chars to "{}"'.format(len(contents), path))
-
-
-def print_mturk_cols(path: str) -> None:
-    """Prints mturk columns for csv file at `path`.
-
-    NOTE: May want to make command line util at some point. Have to figure out how to do
-    this.
-    """
-    df = pd.read_csv(path)
-    inputs, outputs, admin = [], [], []
-    for c in df.columns:
-        if c.startswith("Input."):
-            inputs.append(c)
-        elif c.startswith("Answer."):
-            outputs.append(c)
-        else:
-            admin.append(c)
-
-    print("Admin:")
-    print(", ".join(sorted(admin)))
-    print()
-    print("Inputs:")
-    for i in sorted(inputs):
-        print(f"\t{i}")
-    print("Outputs:")
-    for o in sorted(outputs):
-        print(f"\t{o}")
 
 
 def main() -> None:
