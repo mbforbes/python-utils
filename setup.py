cb36fea85f803e84caa9a829281cc50f1983e11b
Max Forbes <m.b.forbes@gmail.com>
2023-07-19 12:24:06 -0700

    failed attempt at including py.typed file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mbforbes_python_utils',
-    version='0.3.0',
+    version='0.3.1',
     author='Maxwell Forbes',
     description="Some tiny python utils so I can be lazier.",
     url="https://github.com/mbforbes/python-utils",
@@ -11,4 +11,5 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     python_requires='>=3.9',
+    data_files=[("", ["py.typed"])],
 )
